021879957f30d684d250643eda7380641d739ae5
Gustavo Santos Teixeira <136706241+GustavoST868@users.noreply.github.com>
2026-03-02 16:22:17 -0300

    Update boolean_search.js

--- boolean_search.js
+++ boolean_search.js
@@ -2,8 +2,10 @@ function SplitWords(docs){
     let word_docs = [];
 
     for( let i= 0; i<docs.length; i++ ){
+        
      let sentence_clean = docs[i].replace(/[.,!?]/g,"");
       word_docs.push(sentence_clean.split(" "));
+        
     }
     
     console.log(word_docs);
