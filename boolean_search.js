bd5a716cbd734469e7080f413c733e519951157f
Gustavo Santos Teixeira <136706241+GustavoST868@users.noreply.github.com>
2026-03-02 15:56:12 -0300

    Implement SplitWords function to process documents
    Added a function to split documents into words.

--- boolean_search.js
+++ boolean_search.js
@@ -1,1 +1,15 @@
+function SplitWords(docs){
+    let word_docs = [];
+
+    for(let i= 0;i<docs.length;i++){
+     let sentence_clean = docs[i].replace(/[.,!?]/g,"");
+      word_docs.push(sentence_clean.split(" "));
+    }
+
+    console.log(word_docs);
+}
+
+let docs = ["Ola tudo bem!","tudo"];
+
+SplitWords(docs);
 
